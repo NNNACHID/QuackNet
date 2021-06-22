b23945f0d96c5dba2b1fed2a83739f54b5bb7256
NNACHID <nicolas.nachid@le-campus-numerique.fr>
2021-06-22 16:58:44 +0200

    last commit

--- src/Controller/QuackController.php
+++ src/Controller/QuackController.php
@@ -3,44 +3,104 @@
 namespace App\Controller;
 
 use App\Entity\Quack;
+use App\Repository\QuackRepository;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\DateTime as ConstraintsDateTime;
 
+/**
+ * @Route("/quack", name="quack")
+ */
+
 class QuackController extends AbstractController
 {
     /**
-     * @Route("/quack", name="quack")
+     * @Route("/", name="index")
      */
-    public function index(): Response
+
+    public function index(QuackRepository $repository): Response
     {
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $Quack = new Quack();
-
-        $Quack->setContent('Quaaaaack');
-        $Quack->setCreatedAt(new \DateTimeImmutable());
-
-        $entityManager->persist($Quack);
-        $entityManager->flush();
-
 
         return $this->render('quack/index.html.twig', [
             'controller_name' => 'QuackController',
+            'quacks' => $repository->findAll()
+
         ]);
     }
-    public function read(): Response
+
+    /**
+    *@Route("/create", name="create")
+     * */
+
+    public function create(): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $quack = new Quack();
+
+        $quack->setContent("WAZAAAAAA");
+        $quack->setCreatedAt(new \DateTimeImmutable());
+
+        $entityManager->persist($quack);
+        $entityManager->flush();
+
+        return $this->render('quack/createQuack.html.twig', [
+            'controller_name' => 'QuackController',
+            'quack' => $quack
+        ]);
 
     }
-    public function update(): Response
-    {
 
+    /**
+     *@Route("/update/{id}", name="update")
+     * */
+
+    public function update(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $quack = $entityManager->getRepository(Quack::class)->find($id);
+
+        if (!$quack) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $quack->setContent("ZEUUUUUUUUUUUHHHH");
+        $entityManager->flush();
+
+
+        return $this->render('quack/updateQuack.html.twig', [
+            'controller_name' => 'QuackController',
+            'quack' => $quack
+        ]);
     }
+
+    /**
+     *@Route("/delete", name="delete")
+     * */
+
     public function delete(): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $quack = $entityManager->getRepository(Quack::class)->find($id);
+
+        if (!$quack) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $quack->setContent("ZEUUUUUUUUUUUHHHH");
+        $entityManager->flush();
+
+
+        return $this->render('quack/updateQuack.html.twig', [
+            'controller_name' => 'QuackController',
+            'quack' => $quack
+        ]);
 
     }
 }
